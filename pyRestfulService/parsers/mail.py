0400fdf6e89337ba64dc64a1b7cdb8e5ae8fe685
teracs <derek.lu@horizonlink.co.jp>
2013-10-08 17:51:42 +0800

    改改mail.py的格式

--- pyRestfulService/parsers/mail.py
+++ pyRestfulService/parsers/mail.py
@@ -7,73 +7,72 @@ import json
 import time
 import config
 def getUnreadCount(user):
-	try:
-		f = open(config.dir_bbshome + "/tmp/"+user+".mailsize","rb")
-		cnt = int(f.read())
-		f.close()
-	except:
-		return -1
-	return cnt
+  try:
+    f = open(config.dir_bbshome + "/tmp/"+user+".mailsize","rb")
+    cnt = int(f.read())
+    f.close()
+  except:
+    return -1
+  return cnt
 
 def setUnreadCount(user,cnt):
-	try:
-		f = open(config.dir_bbshome + "/tmp/"+user+".mailsize","wb")
-		f.write(str(cnt))
-		f.close()
-	except:
-		return -1
-	return cnt
+  try:
+    f = open(config.dir_bbshome + "/tmp/"+user+".mailsize","wb")
+    f.write(str(cnt))
+    f.close()
+  except:
+    return -1
+  return cnt
 
-DIRstruct 	=	["80s",		"76s",		"I",	"80s",		"I",		"4s",		"I",	"I"		]
-order		=	["filename",	"owner",	"pnum",	"title",	"level",	"accessed",	"id",	"reid"	]
-
+DIRstruct   =  ["80s",    "76s",    "I",  "80s",    "I",    "4s",    "I",  "I"    ]
+order    =  ["filename",  "owner",  "pnum",  "title",  "level",  "accessed",  "id",  "reid"  ]
 
 def getMailList(id):
-	return _getFileHeaders(id)
+  return _getFileHeaders(id)
 
 def sendMail(idfrom,idto,title,content):
-	fileid = int(time.time())
-	while "M.%d.A"%fileid in getMailList(idto):
-		fileid +=1
-	f = open(config.dir_bbshome + "/mail/%s/%s/%s/%s" \
-		%(idto[0].upper(),idto[1].upper(),idto,"M.%d.A"%fileid), "wb")
-	f.write(content)
-	f.close()
-	f = open(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(idto[0].upper(),idto[1].upper(),idto),"ab")
-	ldata = ["M.%d.A"%fileid,"%s (%s)"%(idfrom,idfrom),0,title,0,"",fileid,fileid]
-	data = functions.packStruct(ldata,DIRstruct,256)
-	f.write(data)
-	f.close()
-	cnt = getUnreadCount(idto)
-	setUnreadCount(idto,cnt+1)
-	return dict(zip(order,ldata))
+  fileid = int(time.time())
+  while "M.%d.A"%fileid in getMailList(idto):
+    fileid +=1
+  f = open(config.dir_bbshome + "/mail/%s/%s/%s/%s" \
+    %(idto[0].upper(),idto[1].upper(),idto,"M.%d.A"%fileid), "wb")
+  f.write(content)
+  f.close()
+  f = open(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(idto[0].upper(),idto[1].upper(),idto),"ab")
+  ldata = ["M.%d.A"%fileid,"%s (%s)"%(idfrom,idfrom),0,title,0,"",fileid,fileid]
+  data = functions.packStruct(ldata,DIRstruct,256)
+  f.write(data)
+  f.close()
+  cnt = getUnreadCount(idto)
+  setUnreadCount(idto,cnt+1)
+  return dict(zip(order,ldata))
 
 def getMail(id,filename):
-	f = open(config.dir_bbshome + "/mail/%s/%s/%s/%s" \
-		%(id[0].upper(),id[1].upper(),id,filename), "rb")
-	content = f.read()
-	f.close()
-	return functions.GBK2UTF(content)	
+  f = open(config.dir_bbshome + "/mail/%s/%s/%s/%s" \
+    %(id[0].upper(),id[1].upper(),id,filename), "rb")
+  content = f.read()
+  f.close()
+  return functions.GBK2UTF(content)  
 
 def _getFileHeaders(id):
-	try:
-		f = open(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(id[0].upper(),id[1].upper(),id),"rb")
-	#print boardFolder + boardName+ os.path.sep +".DIR"
-	except:
-		return []
-	data = f.read()
-	data = functions.parseStruct(DIRstruct,order,256,data)
-	f.close()
-	for thePost in data:
-		endflag = thePost["filename"].find("\0")
-		if endflag != -1:
-			thePost["filename"] = thePost["filename"][0:endflag]
-	return data	
+  try:
+    f = open(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(id[0].upper(),id[1].upper(),id),"rb")
+  #print boardFolder + boardName+ os.path.sep +".DIR"
+  except:
+    return []
+  data = f.read()
+  data = functions.parseStruct(DIRstruct,order,256,data)
+  f.close()
+  for thePost in data:
+    endflag = thePost["filename"].find("\0")
+    if endflag != -1:
+      thePost["filename"] = thePost["filename"][0:endflag]
+  return data  
 
 if __name__ == "__main__":
-	#print setUnreadCount("FirebirdAPI",2)
-	#print getUnreadCount("FirebirdAPI")
-	#print json.dumps(getMailList("scaret"))
-	mail = sendMail("FirebirdAPI","scaret","HIHI","PUPU")
-	print mail
-	print getMail("scaret",mail["filename"])
+  #print setUnreadCount("FirebirdAPI",2)
+  #print getUnreadCount("FirebirdAPI")
+  #print json.dumps(getMailList("scaret"))
+  mail = sendMail("FirebirdAPI","scaret","HIHI","PUPU")
+  print mail
+  print getMail("scaret",mail["filename"])
