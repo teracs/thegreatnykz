ebba7e8f56a86e6a0dea7d58c6849e99abda8e59
teracs <derek.lu@horizonlink.co.jp>
2013-10-08 18:40:14 +0800

    写了mail的矮矬穷函数。并且分配给了序号

--- pyRestfulService/parsers/mail.py
+++ pyRestfulService/parsers/mail.py
@@ -1,3 +1,4 @@
+#coding:utf8
 import struct
 import sys
 import os
@@ -27,8 +28,33 @@ def setUnreadCount(user,cnt):
 DIRstruct   =  ["80s",    "76s",    "I",  "80s",    "I",    "4s",    "I",  "I"    ]
 order    =  ["filename",  "owner",  "pnum",  "title",  "level",  "accessed",  "id",  "reid"  ]
 
-def getMailList(id):
+# 高帅富函数
+def getAllMailList(id):
   return _getFileHeaders(id)
+
+# 矮矬穷函数
+def getMailList(id,startfrom1 = 99999, count = 20):
+  try:
+    f = open(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(id[0].upper(),id[1].upper(),id),"rb")
+  #print boardFolder + boardName+ os.path.sep +".DIR"
+  except:
+    return []
+  if startfrom1 < 0 or startfrom1 > countMail(id):
+    startfrom1 = countMail(id) - count +1
+  data = f.read((startfrom1 - 1)* 256)
+  data = f.read(count * 256)
+  if not data:
+    return []
+  data = functions.parseStruct(DIRstruct,order,256,data)
+  f.close()
+  i = 0
+  for thePost in data:
+    thePost["index"] = startfrom1 + i
+    i += 1
+    endflag = thePost["filename"].find("\0")
+    if endflag != -1:
+      thePost["filename"] = thePost["filename"][0:endflag]
+  return data
 
 def sendMail(idfrom,idto,title,content):
   fileid = int(time.time())
@@ -52,7 +78,13 @@ def getMail(id,filename):
     %(id[0].upper(),id[1].upper(),id,filename), "rb")
   content = f.read()
   f.close()
-  return functions.GBK2UTF(content)  
+  return functions.GBK2UTF(content)
+
+# 返回最大邮件号
+def countMail(id):
+  dirfilesize = os.path.getsize(config.dir_bbshome + "/mail/%s/%s/%s/.DIR"%(id[0].upper(),id[1].upper(),id))
+  count = dirfilesize/256
+  return count
 
 def _getFileHeaders(id):
   try:
@@ -67,12 +99,8 @@ def _getFileHeaders(id):
     endflag = thePost["filename"].find("\0")
     if endflag != -1:
       thePost["filename"] = thePost["filename"][0:endflag]
-  return data  
+  return data
 
 if __name__ == "__main__":
-  #print setUnreadCount("FirebirdAPI",2)
-  #print getUnreadCount("FirebirdAPI")
-  #print json.dumps(getMailList("scaret"))
-  mail = sendMail("FirebirdAPI","scaret","HIHI","PUPU")
-  print mail
-  print getMail("scaret",mail["filename"])
+  print countMail("Teracs")
+  print getMailList("Teracs")
